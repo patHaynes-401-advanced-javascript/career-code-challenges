286f5f0851fa952d1bdf0ca0620dc6de0a067248
Pat Haynes <pathaynes32@gmail.com>
2019-10-23 09:32:24 -0700

    changed to UTC string

--- __tests__/date-adder.test.js
+++ __tests__/date-adder.test.js
@@ -6,49 +6,49 @@ describe('date adding and testing', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const secondsAdded = '10s';
     const seconds = dateAdder(date, secondsAdded);
-    expect(seconds.toString()).toEqual('Sun Jan 20 2019 10:33:40 GMT-0800 (Pacific Standard Time)');
+    expect(seconds.toUTCString()).toEqual('Sun, 20 Jan 2019 18:33:40 GMT');
   });
 
   it('adds minutes to the date', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const minutesAdded = '10m';
     const minutes = dateAdder(date, minutesAdded);
-    expect(minutes.toString()).toEqual('Sun Jan 20 2019 10:43:30 GMT-0800 (Pacific Standard Time)');
+    expect(minutes.toUTCString()).toEqual('Sun, 20 Jan 2019 18:43:30 GMT');
   });
 
   it('adds hours to the date', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const hoursAdded = '1h';
     const hours = dateAdder(date, hoursAdded);
-    expect(hours.toString()).toEqual('Sun Jan 20 2019 11:33:30 GMT-0800 (Pacific Standard Time)');
+    expect(hours.toUTCString()).toEqual('Sun, 20 Jan 2019 19:33:30 GMT');
   });
 
   it('adds days to the date', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const daysAdded = '1d';
     const days = dateAdder(date, daysAdded);
-    expect(days.toString()).toEqual('Mon Jan 21 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
+    expect(days.toUTCString()).toEqual('Mon, 21 Jan 2019 18:33:30 GMT');
   });
 
   it('adds weeks to the date', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const weeksAdded = '1w';
     const weeks = dateAdder(date, weeksAdded);
-    expect(weeks.toString()).toEqual('Sun Jan 27 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
+    expect(weeks.toUTCString()).toEqual('Sun, 27 Jan 2019 18:33:30 GMT');
   });
 
   it('adds months to the date', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const hoursAdded = '1M';
     const hours = dateAdder(date, hoursAdded);
-    expect(hours.toString()).toEqual('Wed Feb 20 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
+    expect(hours.toUTCString()).toEqual('Wed, 20 Feb 2019 18:33:30 GMT');
   });
 
   it('adds years to the date', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const yearsAdded = '1y';
     const years = dateAdder(date, yearsAdded);
-    expect(years.toString()).toEqual('Mon Jan 20 2020 10:33:30 GMT-0800 (Pacific Standard Time)');
+    expect(years.toUTCString()).toEqual('Mon, 20 Jan 2020 18:33:30 GMT');
   });
 
 
