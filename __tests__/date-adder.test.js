4b2cdd912a060266be94e016d5e6e33c5e9a920f
Pat Haynes <pathaynes32@gmail.com>
2019-10-22 17:58:08 -0700

    tests pass

--- __tests__/date-adder.test.js
+++ __tests__/date-adder.test.js
@@ -16,26 +16,40 @@ describe('date adding and testing', () => {
     expect(minutes.toString()).toEqual('Sun Jan 20 2019 10:43:30 GMT-0800 (Pacific Standard Time)');
   });
 
-  it.skip('adds hours to the date', () => {
-
+  it('adds hours to the date', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const hoursAdded = '1h';
+    const hours = dateAdder(date, hoursAdded);
+    expect(hours.toString()).toEqual('Sun Jan 20 2019 11:33:30 GMT-0800 (Pacific Standard Time)');
   });
 
-  it.skip('adds days to the date', () => {
-
+  it('adds days to the date', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const daysAdded = '1d';
+    const days = dateAdder(date, daysAdded);
+    expect(days.toString()).toEqual('Mon Jan 21 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
   });
 
-  it.skip('adds weeks to the date', () => {
-
+  it('adds weeks to the date', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const weeksAdded = '1w';
+    const weeks = dateAdder(date, weeksAdded);
+    expect(weeks.toString()).toEqual('Sun Jan 27 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
   });
 
-  it.skip('adds months to the date', () => {
-
+  it('adds months to the date', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const hoursAdded = '1M';
+    const hours = dateAdder(date, hoursAdded);
+    expect(hours.toString()).toEqual('Wed Feb 20 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
   });
 
-  it.skip('adds years to the date', () => {
-
+  it('adds years to the date', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const yearsAdded = '1y';
+    const years = dateAdder(date, yearsAdded);
+    expect(years.toString()).toEqual('Mon Jan 20 2020 10:33:30 GMT-0800 (Pacific Standard Time)');
   });
-
 
 
 });
