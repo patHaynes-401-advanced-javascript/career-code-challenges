349ece0e4fb37d4bf71e98433d6d1ceeb64e43b3
Pat Haynes <pathaynes32@gmail.com>
2019-10-26 14:18:44 -0700

    working on difference test

--- __tests__/set.test.js
+++ __tests__/set.test.js
@@ -1,28 +1,57 @@
+const Set = require('../challenges/set');
+
 
 describe('checks to see if set class works', () => {
-//instance methods
-  it.skip('adds an item to set', () => {
-
+  //instance methods
+  it('creates a set', () => {
+    const set = new Set([1, 2, 3]);
+    expect(set.array).toEqual([1, 2, 3]);
   });
 
-  it.skip('removes an item to set', () => {
-
+  it('can add to a set', () => {
+    const set = new Set([1, 2, 3]);
+    set.add(4);
+    expect(set.array).toEqual([1, 2, 3, 4]);
   });
 
-  it.skip('has an item in set', () => {
-
+  it('wont add a duplicate item', () => {
+    const set = new Set([1, 2, 3]);
+    set.add(1);
+    expect(set.array).toEqual([1, 2, 3]);
   });
 
-  it.skip('takes a set, returns a new set with the intersection of those sets', () => {
-
+  it('removes an item to set', () => {
+    const set = new Set([1, 2, 3]);
+    set.remove(1);
+    expect(set.array).toEqual([2, 3]);
   });
 
-  it.skip('takes a set, returns a new set with the union of those sets', () => {
-
+  it('has an item in set', () => {
+    const set = new Set([1, 2, 3]);
+    const answer = set.has(3);
+    expect(answer).toEqual(true);
   });
 
-  it.skip('takes a set, returns a new set with the union of those sets', () => {
+  it('takes a set, returns a new set with the intersection of those sets', () => {
+    const set1 = new Set([1, 2, 3]);
+    const set2 = new Set([2, 3, 6]);
+    const answer = set1.intersection(set2);
+    console.log(answer.array);
+    expect(answer.array).toEqual([2, 3]);
+  });
 
+  it('takes a set, returns a new set with the union of those sets', () => {
+    const set1 = new Set([1, 2, 3]);
+    const set2 = new Set([2, 3, 6]);
+    const answer = set1.union(set2);
+    expect(answer.array).toEqual([1, 2, 3, 6]);
+  });
+
+  it('takes a set, returns a new set with the difference of those sets', () => {
+    const set1 = new Set([1, 2, 3]);
+    const set2 = new Set([2, 3, 6]);
+    const answer = set1.difference(set2);
+    expect(answer.array).toEqual([1, 6]);
   });
 
   //static methods
@@ -34,7 +63,7 @@ describe('checks to see if set class works', () => {
 
   });
 
-  it.skip('takes two sets, and returns a new set with the difference of those sets' () {
+  it.skip('takes two sets, and returns a new set with the difference of those sets', () => {
 
   });
 
